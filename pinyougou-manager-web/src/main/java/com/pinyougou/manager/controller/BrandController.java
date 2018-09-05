99172bef91709e75de5be3dfd90776e76262f37a
SZMOFEI <635071732@qq.com>
2018-09-05 23:30:29 +0800

    [新增]：[品牌管理]->品牌新增

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -1,9 +1,11 @@
 package com.pinyougou.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.pinyougou.entity.PageResult;
+import entity.PageResult;
 import com.pinyougou.pojo.Brand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,21 @@ public class BrandController {
     @RequestMapping("/findPage")
     public PageResult findPage(int page,int rows) {
         return brandService.findPage(page,rows);
+    }
+
+    /**
+     * 增加
+     * @param brand 品牌
+     * @return Result
+     */
+    @RequestMapping("/add")
+    public Result addBrand(@RequestBody Brand brand){
+        try {
+            brandService.addBrand(brand);
+            return new Result(true,"增加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"添加失败");
+        }
     }
 }
