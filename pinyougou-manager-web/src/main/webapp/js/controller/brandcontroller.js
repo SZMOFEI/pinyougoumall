3e590974cb77763870d4c2b20e47b32e3848db7e
SZMOFEI <635071732@qq.com>
2018-09-17 22:03:57 +0800

    [修改]：[品牌管理]->控制器继承

--- pinyougou-manager-web/src/main/webapp/js/controller/brandcontroller.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandcontroller.js
@@ -1,33 +1,7 @@
-app.controller('brandcontroller' ,function($scope,brandService){
+app.controller('brandcontroller' ,function($scope,$controller,brandService){
 
+    $controller('baseController',{$scope:$scope});
 
-    $scope.reloadList = function () {
-        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
-    }
-//分页控件配置
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function () {
-            $scope.reloadList();
-        }
-    };
-
-
-
-    //更新复选框
-    $scope.selectIds = [];
-    $scope.updateSelection = function ($event, id) {
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);
-        } else {
-            var index = $scope.selectIds.indexOf(id);
-            $scope.selectIds.splice(index);
-        }
-
-    }
     <!--执行分页方法-->
     $scope.findPage = function (page, rows) {
         brandService.findPage().success(
