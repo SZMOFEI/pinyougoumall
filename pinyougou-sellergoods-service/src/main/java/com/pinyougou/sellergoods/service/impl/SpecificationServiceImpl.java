ebbbf63d365000bc8e6de51bbe5fa92ca5875105
SZMOFEI <635071732@qq.com>
2018-09-17 23:33:03 +0800

    [新增]：[规格管理]->1.代码生成器2.规格列表查询

---
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SpecificationServiceImpl.java
@@ -0,0 +1,100 @@
+package com.pinyougou.sellergoods.service.impl;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.SpecificationMapper;
+import com.pinyougou.pojo.Specification;
+import com.pinyougou.pojo.SpecificationExample;
+import com.pinyougou.pojo.SpecificationExample.Criteria;
+import com.pinyougou.sellergoods.service.SpecificationService;
+import entity.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class SpecificationServiceImpl implements SpecificationService {
+
+	@Autowired
+	private SpecificationMapper specificationMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<Specification> findAll() {
+		return specificationMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<Specification> page=   (Page<Specification>) specificationMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(Specification specification) {
+		specificationMapper.insert(specification);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(Specification specification){
+		specificationMapper.updateByPrimaryKey(specification);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public Specification findOne(Long id){
+		return specificationMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			specificationMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(Specification specification, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		SpecificationExample example=new SpecificationExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(specification!=null){			
+						if(specification.getSpecName()!=null && specification.getSpecName().length()>0){
+				criteria.andSpecNameLike("%"+specification.getSpecName()+"%");
+			}
+	
+		}
+		
+		Page<Specification> page= (Page<Specification>)specificationMapper.selectByExample(example);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+	
+}
